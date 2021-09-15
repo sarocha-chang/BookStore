d4ce06092e951c0c330a3496a31029859a6da2dd
artjitt9611 <jittaphon_jankham@cmu.ac.th>
2021-09-15 19:46:31 +0700

    Add search function

--- server/asset/server/all/search.js
+++ server/asset/server/all/search.js
@@ -14,49 +14,12 @@ app.use(
 );
 
 app.get("/:name", async (request, response) => {
-    let data = await Book.find()
-    let array = [...request.params.name]
-    let name = ""
-    let store = []
-    array.forEach((intel,count) =>{
-        name += intel
-        for(let obj of data){
-            for(let index = 1;index<=obj.name.length;index++){
-                let name_obj = obj.name.substring(0,index)
-                if(name_obj == name){
-                    store.push(obj)
-                    break;
-                }
-            }
-        }
-    })
-    store.map((data) =>{
-        
-    })
-	response.status(200).json();
+     console.log(request.params.name)
+     let x = await Book.find()
+     let find = x.filter(p =>(p.name.includes(request.params.name)))
+     console.log(find)
+     response.status(200).json(find);
 });
 
-// search = (item) => {
-// 	if (item) {
-//         let array = []
-//         let x = stock.entries();
-//         stock.forEach(() =>{
-//             let y = x.next().value
-//             array.push(y)
-//         })
-//         array = array.filter((data) =>{
-//             length = item.length
-//             let y = data[0].substring(0,length)
-//             return item == y
-//         })
-//         if(array.length > 0) array = [`found ${array.length} item`,array]
-//         else if(array.length === 0) array = ['Not found','Not found']
-//         console.log(array);
-// 		return array
-// 	} else {
-// 		text = "Please input item.";
-// 		return text;
-// 	}
-// };
 
 module.exports = app;
