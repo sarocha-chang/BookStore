540c2fb4f9835177449051333d84ff42245b0234
kaponnon <kaphon_s@cmu.ac.th>
2021-09-16 23:19:29 +0700

    edit getCartapi

--- server/asset/function/getCart.js
+++ server/asset/function/getCart.js
@@ -1,4 +1,4 @@
-/*const Book = require("../../config/collection/Book");
+const Book = require("../../config/collection/Book");
 const Customer = require("../../config/collection/Customer");
 const Buy = require("../../config/collection/Buy");
 const Receipt = require("../../config/collection/Receipt");
