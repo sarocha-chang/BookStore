7ba44813782e3b2b404a78b37d94c0064882a0c7
sarocha-chang <sarocha_chang@cmu.ac.th>
2021-09-17 16:44:24 +0700

    edit varible

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,44 +1,32 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { Switch, Route,  Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import Sidebar from "./admin/Sidebar";
 import ContainerAd from "./admin/Container";
 import ShowBook from "./admin/ShowBook";
 import GlobalStyle from "./GlobalStyle";
 function App() {
-  const [product, SetProduct] = useState([]);
-  useEffect(() => {
-    axios.get("http://localhost:3001/show").then((res) => {
-      SetProduct(res.data);
-    });
-  }, []);
-  console.log();
-
   return (
     <>
-    <GlobalStyle />
+      <GlobalStyle />
+      
       <Switch>
+
         <Route path="/HomeAdmin">
           <Sidebar />
           <ContainerAd>
             <ShowBook />
           </ContainerAd>
         </Route>
+
         <Route path="/AddBookAd">
           <Sidebar />
-          <ContainerAd>
-          </ContainerAd>
-          </Route>
-		<Route path="/">
-			<Link to="/HomeAdmin">Admin</Link>
-		</Route>
-      </Switch>
+          <ContainerAd></ContainerAd>
+        </Route>
 
-      {/* <div className="App">
-			{product.map((data) => (
-				<h1>{data.name}</h1>
-			))}
-		</div> */}
+        <Route path="/">
+          <Link to="/HomeAdmin">Admin</Link>
+        </Route>
+
+      </Switch>
     </>
   );
 }
