4b4cf6a63ee5d81b072130835d3445ef858020af
sarocha-chang <sarocha_chang@cmu.ac.th>
2021-09-18 01:41:05 +0700

    ADD BOOK FINISH

--- bookstore/src/GlobalStyle.js
+++ bookstore/src/GlobalStyle.js
@@ -1,6 +1,7 @@
 import {createGlobalStyle} from "styled-components";
 const GlobalStyle = createGlobalStyle`
 font-family: 'IBM Plex Sans Thai', sans-serif;
+margin: 0;
 `;
 
 export default GlobalStyle;
--- bookstore/src/admin/AddBook.js
+++ bookstore/src/admin/AddBook.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import styled from "styled-components";
 import { useState } from "react";
+import {Link} from "react-router-dom";
+import Swal from 'sweetalert2';
 
-function AddBook({className}) {
+function AddBook({ className }) {
   const [name, setName] = useState("");
   const [author, setAuthor] = useState("");
   const [description, setDescription] = useState("");
@@ -34,102 +36,237 @@ function AddBook({className}) {
       .catch((error) => {
         console.log(error);
       });
+    alertSubmit(imageUrl);
   }
 
   return (
     <div className={className}>
-      <h1 className="top"> เพิ่มหนังสือใหม่ </h1>{" "}
+      <h1 className="top"> เพิ่มหนังสือใหม่ </h1>
       <form className="add">
-        <div className="form-group">
-          <label> ชื่อหนังสือ: </label>{" "}
+          <div className="row">
+            <div className="col-10">
+          <label> ชื่อหนังสือ: </label>
+          </div>
+          <div className="col-90">
           <input
             type="text"
             placeholder="กรุณากรอกชื่อหนังสือ"
+            className="medium"
             onChange={(event) => setName(event.target.value)}
           />
-        </div>{" "}
-        <div className="form-group">
-          <label> ชื่อผู้แต่ง: </label>{" "}
+        </div>
+        </div>
+        <div className="row">
+            <div className="col-10">
+          <label> ชื่อผู้แต่ง: </label>
+          </div>
+          <div className="col-90">
           <input
             type="text"
             placeholder="กรุณากรอกชื่อผู้เขียน"
+            className="medium"
             onChange={(event) => setAuthor(event.target.value)}
           />
-        </div>{" "}
-        <div className="form-group">
-          <label> คำอธิบาย: </label>{" "}
-          <input
+        </div>
+        </div>
+        <div className="row">
+            <div className="col-10">
+          <label> คำอธิบาย: </label>
+          </div>
+          <div className="col-90">
+          <textarea
             type="text"
-            placeholder="กรุณากรอกคำอธิบาย"
+            placeholder="กรุณากรอกคำอธิบาย. . . . . . . . . ."
+            className="long"
             onChange={(event) => setDescription(event.target.value)}
           />
-        </div>{" "}
-        <div className="form-group">
-          <label> ประเภท: </label>{" "}
+        </div>
+        </div>
+        <div className="row">
+            <div className="col-10">
+          <label> ประเภท: </label>
+          </div>
+          <div className="col-90">
           <select onChange={(event) => setType(event.target.value)}>
-            <option> นวนิยาย </option> <option> ภาษา </option>{" "}
-            <option> ความรู้ทั่วไป </option> <option> พัฒนาตนเอง </option>{" "}
-          </select>{" "}
-        </div>{" "}
-        <div className="form-group">
-          <label> ราคา: </label>{" "}
+            <option> นวนิยาย </option> <option> ภาษา </option>
+            <option> ความรู้ทั่วไป </option> <option> พัฒนาตนเอง </option>
+          </select>
+        </div>
+        </div>
+        <div className="row">
+            <div className="col-10">
+          <label> ราคา: </label>
+          </div>
+          <div className="col-90">
           <input
             type="text"
             placeholder="กรุณากรอกราคา"
+            className="short"
             onChange={(event) => setPrice(event.target.value)}
           />
-        </div>{" "}
-        <div className="form-group">
-          <label> จำนวน: </label>{" "}
+        </div>
+        </div>
+        <div className="row">
+            <div className="col-10">
+          <label> จำนวน: </label>
+          </div>
+          <div className="col-90">
           <input
             type="text"
             placeholder="กรุณากรอกจำนวนหนังสือ"
+            className="short"
             onChange={(event) => setQuantity(event.target.value)}
           />
-        </div>{" "}
-        <div className="form-group">
-          <label> ลิงค์รูปภาพ: </label>{" "}
+        </div>
+        </div>
+        <div className="row">
+            <div className="col-10">
+          <label> ลิงค์รูปภาพ: </label>
+          </div>
+          <div className="col-90">
           <input
             type="text"
             placeholder="กรุณากรอกลิงค์รูปภาพ"
+            className="medium"
             onChange={(event) => setImageUrl(event.target.value)}
           />
-        </div>{" "}
-        <div className="form-group">
-          <label> สถานะ: </label>{" "}
+        </div>
+        </div>
+        <div className="row">
+          <div className="col-10">
+          <label> สถานะ: </label>
+          </div>
+          <div className="col-90">
           <select onChange={(event) => setStatus(event.target.value)}>
-            <option> ปกติ </option> <option> มาใหม่ </option>{" "}
-            <option> ยอดนิยม </option>{" "}
-          </select>{" "}
-        </div>{" "}
-        <div className="form-group">
-          <button type="cancel"> ยกเลิก </button>{" "}
-          <button type="submit" onClick={onSubmit}>
-            {" "}
-            ยืนยัน{" "}
-          </button>{" "}
-        </div>{" "}
-      </form>{" "}
+            <option> ปกติ </option> <option> มาใหม่ </option>
+            <option> ยอดนิยม </option>
+          </select>
+        </div>
+        </div>
+        <div className="butt">
+          <Link to="./HomeAdmin"><button type="cancel" className="cancel"> ยกเลิก </button></Link>
+          <button type="submit" className="submit" onClick={onSubmit}>
+            ยืนยัน
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
+
+function alertSubmit(imageUrl){
+  Swal.fire({
+    title: 'Success!',
+    text: 'Your book has been added',
+    confirmButtonColor: "#005488",
+    imageUrl: imageUrl,
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
+}
 export default styled(AddBook)`
   font-family: "IBM Plex Sans Thai", sans-serif;
-  margin: 0px;
   h1.top {
     font-family: "IBM Plex Sans Thai", sans-serif;
     font-size: 26px;
     text-align: center;
-    margin-bottom: 1.5rem;
+    margin-top: 0;
+    padding-top: 45px;
+    margin-bottom: 2rem;
   }
-  div.form-group {
+
+  form.add {
+    border: 1px solid #ccc;
+    width: 80%;
+    border-radius: 5px;
+    padding: 1rem;
+    margin-left: 8rem;
+    margin-bottom: 4rem;
+  }
+  div.row {    
+    padding: 15px 10px 15px 10px;
+    .col-10 {
+      float: left;
+      width: 10%;
+      margin-top: 16px;
+
+    }
+    .col-90 {
+      float: left;
+      width: 90%;
+      margin-top: 16px;
+
+    }
     label {
       font-size: 20px;
       font-weight: bold;
+      vertical-align:top;
+      display: inline-block;
     }
-    input {
-      width: 80%;
+    input,textarea {
+      font-family: "IBM Plex Sans Thai", sans-serif;
       border-radius: 5px;
+      padding: 8px;
+      font-size: 16px;
+      border: 1px solid #ccc;
     }
+    input.short{
+      width: 20%;
+    }input.medium{
+      width: 55%;
+    }
+    textarea.long{
+      width: 65%;
+      padding-bottom: 8rem;
+    }
+    select {
+      font-family: "IBM Plex Sans Thai", sans-serif;
+      width: 12%;
+      border-radius: 5px;
+      padding: 8px;
+      font-size: 16px;
+      border: 1px solid #ccc;
+
+    } 
   }
+  div.butt {
+    text-align: center;
+
+  }
+  div.butt button {
+    margin-top: 3rem;
+    text-align: center;
+    font-family: "IBM Plex Sans Thai", sans-serif;
+    font-size: 18px;
+    padding: 5px;
+    width: 8%;
+    color: #fff;
+    border-radius: 10px;
+    margin-bottom: 1rem;
+}
+button.cancel{
+  border: 1px solid #EC5858;
+  background-color: #EC5858;
+  margin-left: 3rem;
+}
+button.submit{
+  border: 1px solid #5E5E5E;
+  background-color: #5E5E5E;
+  margin-left: 3rem;
+  margin-right: 3rem;
+}
+button.submit:hover{
+  background-color: #1DB02C;
+  border: 1px solid #1DB02C;
+  transition: all 0.3s;
+}
+button.cancel:hover{
+  background-color: #FFC531;
+  border: 1px solid #FFC531;
+  transition: all 0.3s;
+}
+}
+
+  
 `;
