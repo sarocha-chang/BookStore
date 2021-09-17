263b343cb3847a5edd0b7bc93b60ee2474e8f6ad
kaponnon <kaphon_s@cmu.ac.th>
2021-09-17 16:31:24 +0700

    Commit npm i

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,39 +1,29 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { Switch, Route,  Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
+
 import Sidebar from "./admin/Sidebar";
 import Container from "./admin/Container";
 import ShowBook from "./admin/ShowBook";
+
 function App() {
-  const [product, SetProduct] = useState([]);
-  useEffect(() => {
-    axios.get("http://localhost:3001/show").then((res) => {
-      SetProduct(res.data);
-    });
-  }, []);
-  console.log();
+	return (
+		<>
+			<Switch>
 
-  return (
-    <>
-      <Switch>
-        <Route path="/HomeAdmin">
-          <Sidebar />
-          <Container>
-            <ShowBook />
-          </Container>
-        </Route>
-		<Route path="/">
-			<Link to="/HomeAdmin">Admin</Link>
-		</Route>
-      </Switch>
+				<Route path="/HomeAdmin">
+					<Sidebar />
+					<Container>
+						<ShowBook />
+					</Container>
+				</Route>
 
-      {/* <div className="App">
-			{product.map((data) => (
-				<h1>{data.name}</h1>
-			))}
-		</div> */}
-    </>
-  );
+				<Route path="/">
+					<Link to="/HomeAdmin">Admin</Link>
+          
+				</Route>
+
+			</Switch>
+		</>
+	);
 }
 
 export default App;
