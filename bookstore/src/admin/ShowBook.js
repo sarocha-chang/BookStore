f38753ffc362355e01c249991946aca4c189b5ad
kaponnon <kaphon_s@cmu.ac.th>
2021-09-17 23:55:50 +0700

    add delete book

--- bookstore/src/admin/ShowBook.js
+++ bookstore/src/admin/ShowBook.js
@@ -1,81 +1,92 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+
 import HomeAdmin from "./HomeAdmin";
-import 'boxicons' ;
-function Books({className}) {
-    const [books, setBook] = useState([]);
+import Context from "../Context";
+import "boxicons";
+
+function Books({ className }) {
+	const [books, setBook] = useState([]);
 	useEffect(() => {
 		axios.get("http://localhost:3001/show").then((res) => {
 			setBook(res.data);
 		});
 	}, []);
-  return (
-      <>
-      <div className={className}>
-          <h1 className="top">ข้อมูลหนังสือ</h1>
-          <table className="ShowBook">
-            <thead>
-                <td className="Topic">รูป</td>
-                <td className="Topic">ชื่อหนังสือ</td>
-                <td className="Topic">ชื่อผู้แต่ง</td>
-                <td className="Topic">คำอธิบาย</td>
-                <td className="Topic">ประเภท</td>
-                <td className="Topic"> ราคา</td>
-                <td className="Topic">จำนวน</td>
-                <td className="Topic">สถานะ</td>
-                <td className="Topic"></td>
-            </thead>
-            <tbody>
-            {books ?
-                books.map((data) => {
-                    return <HomeAdmin key={data._id} data={data} />
-                })
-            :(
-				<div>Loading products....</div>
-			)}
-        </tbody>
-        </table>
-        </div>
-    </>
-  );
+
+	return (
+		<>
+			<div className={className}>
+				<h1 className="top">ข้อมูลหนังสือ</h1>
+				<table className="ShowBook">
+					<thead>
+						<tr>
+							<th>รูป</th>
+							<th>ชื่อหนังสือ</th>
+							<th>ชื่อผู้แต่ง</th>
+							<th>คำอธิบาย</th>
+							<th>ประเภท</th>
+							<th> ราคา</th>
+							<th>จำนวน</th>
+							<th>สถานะ</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						{books ? (
+							books.map((data) => {
+								return (
+									<Context.Provider value={[books,setBook]} key={data._id}>
+										{<HomeAdmin  data={data} />}
+									</Context.Provider>
+								);
+							})
+						) : (
+							<div>Loading products....</div>
+						)}
+					</tbody>
+				</table>
+			</div>
+		</>
+	);
 }
 
-Books.propTypes={
-    className: PropTypes.string.isRequired,
-    books: PropTypes.object.isRequired
-}
+Books.propTypes = {
+	className: PropTypes.string.isRequired,
+	books: PropTypes.node,
+
+};
 
 export default styled(Books)`
-margin-bottom: 50px;
-    width: 98%;
-    h1.top{
-        font-family: 'IBM Plex Sans Thai', sans-serif;
-        padding-top: 40px;
-        margin-top: 0px;
-        font-size: 26px;
-        text-align: center;
-        margin-bottom: 4rem;
-    }
-    .ShowBook{
-            border-collapse: collapse;
-            border-radius: 15px;
-            width: 100%;
-            table-layout: fixed;
-            td.Topic{
-                text-align: center;
-                font-size: 20px;
-                font-weight: bold;    
-                padding-bottom: 30px;
-                border-bottom: 1px solid #e5e5e5;
-            }
-        
-            td{
-                color: black;
-                font-size: 18px;
-                text-align:center;
-                padding: 25px 10px 10px 10px;
-            }
-        }
-    `;
+	margin-bottom: 50px;
+	width: 98%;
+	h1.top {
+		font-family: "IBM Plex Sans Thai", sans-serif;
+		padding-top: 40px;
+		margin-top: 0px;
+		font-size: 26px;
+		text-align: center;
+		margin-bottom: 4rem;
+	}
+	.ShowBook {
+		border-collapse: collapse;
+		border-radius: 15px;
+		width: 100%;
+		table-layout: fixed;
+		thead tr th {
+			text-align: center;
+			font-size: 20px;
+			font-weight: bold;
+			padding-bottom: 30px;
+			border-bottom: 1px solid #e5e5e5;
+		}
+
+		td {
+			color: black;
+			font-size: 18px;
+			text-align: center;
+			padding: 25px 10px 10px 10px;
+		}
+	}
+`;
