204973214436bb8a73a06a9aef5cad5b889e95ab
kaponnon <kaphon_s@cmu.ac.th>
2021-09-18 14:40:38 +0700

    change

--- server/asset/routers/admin/delete_book.js
+++ server/asset/routers/admin/delete_book.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const app = express.Router();
 
 const Book = require("../../../config/collection/Book");
 
