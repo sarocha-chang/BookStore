167426c7c55d3a02f08a58c4beb34f7650ab41e0
sarocha-chang <sarocha_chang@cmu.ac.th>
2021-09-20 22:56:30 +0700

    delete cart in home

--- bookstore/src/home/Navbar1.js
+++ bookstore/src/home/Navbar1.js
@@ -9,9 +9,6 @@ export default function Navbar1() {
                 <nav>
                     <ul class="nav-link">
                         <Link to="/Login"><li><a href="#">login</a></li></Link>
-                        <li><Link to="/Cart" className="cart">
-                            <box-icon name="shopping-bag" color="#fff"></box-icon>
-                        </Link></li>
                     </ul>
                 </nav>
             </header>
