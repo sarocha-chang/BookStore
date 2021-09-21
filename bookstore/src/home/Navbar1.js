181cbb8d841291aa8a5cf66bc4b0463ec8ee890e
sirawat <poomfun@gmail.com>
2021-09-21 18:55:18 +0700

    commit

--- bookstore/src/home/Navbar1.js
+++ bookstore/src/home/Navbar1.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 export default function Navbar1() {
+    function logOut(){
+        localStorage.removeItem("username");
+        
+      }
     return (
         <>
             <header>
@@ -10,6 +14,9 @@ export default function Navbar1() {
                 <Link to={localStorage.getItem("username") ? '#':'/login' }    className="login" >
           {localStorage.getItem("username") ?localStorage.getItem("username"):'เข้าสู่ระบบ' }   
           </Link>
+          {localStorage.getItem("username")?
+          <Link onClick={logOut} to="/Home">Logout</Link>:null
+        }
                 </nav>
             </header>
         </>
